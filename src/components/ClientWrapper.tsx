be0af7b00c1aa4f75dbe96d9164348baf93d591f
Omega4T <omegawidyatmaja@gmail.com>
2025-10-16 17:37:11 +0700

    Feat: Fix deployment error

--- src/components/ClientWrapper.tsx
+++ src/components/ClientWrapper.tsx
@@ -13,11 +13,9 @@ const ClientWrapper = ({ children }: ClientWrapperProps) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
-      mouseMultiplier: 1,
+      smoothWheel: true,
       smoothTouch: false,
       touchMultiplier: 2,
-      infinite: false,
     });
 
     // Fungsi untuk mengupdate Lenis setiap frame
